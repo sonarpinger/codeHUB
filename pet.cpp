fc4a303ba5089e8c0dd017fdbb5b2838f56ccdf2
brandonGHUB <112513361+brandonGHUB@users.noreply.github.com>
2022-09-20 13:45:40 -0700

    Add pet example starter files

---
+++ pet.cpp
@@ -0,0 +1,36 @@
+//include header file
+
+
+//default constructor
+
+//copy constructor
+
+//parameterized constructor
+
+
+
+//eat method
+//:: is scope resolution operator. It tells which class methods object can call
+
+//utterance method
+
+//setLegs
+
+
+//getLegs
+
+//setTail
+
+
+//getTail
+
+
+//setSpecies
+
+
+//getSpecies
+
+//setName
+
+
+//getName
