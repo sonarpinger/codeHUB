882010aa89715dd945afb8577c7b1442baedf9b9
brandonGHUB <112513361+brandonGHUB@users.noreply.github.com>
2022-10-11 14:35:09 -0700

    implement shape-triangle relationship

--- DemoCode/week7/demoStarterFiles/shape/shape.h
+++ DemoCode/week7/demoStarterFiles/shape/shape.h
@@ -1,4 +1,24 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+using namespace std;
+#include <iostream>
+#include <string>
 
+class Shape{
+   protected:
+      int sides;
+      float area, perimeter;
+   public:
+      Shape();
+      Shape(float, float);
+      Shape(const Shape&);
 
+      float getArea();
+      float getPerimeter();
+
+      void setArea(float);
+      void setPerimeter(float);
+};
    //declare properties/methods
    //including default and copy constructor, and getP getA
+#endif
