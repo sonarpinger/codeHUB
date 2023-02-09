64bf2dd4270ca2a084de07019e9c0ec3a5720155
brandonGHUB <112513361+brandonGHUB@users.noreply.github.com>
2023-02-09 10:12:55 -0800

    oveflow function

--- ALUemul/alu.cpp
+++ ALUemul/alu.cpp
@@ -4,9 +4,24 @@ uint32_t ALU::add(uint32_t op1, uint32_t op2){
     // returns the bitwise of the both 32 bit unsigned numbers combined
     // with the bitwise and of the both numbers and returns it, emulating
     // the functionality of the low level registers
-    return (op1 | op2) + (op1 & op2);
+    uint32_t res = (op1 | op2) + (op1 & op2);
+    // calls overflow detector function
+    didOverflow(op1, op2, res, 1);
+    return res;
 }
 
-bool ALU::didOverflow(uint32_t op1, uint32_t op2, uint32_t res, int func){
-    
+// method that takes in operands, result of ALU function, and the ALU function code
+// and determines if an overflow event hath occured
+void ALU::didOverflow(uint32_t op1, uint32_t op2, uint32_t res, int func){
+    switch (func)
+    {
+    case 1:
+        if(res > 65335){
+            std::cout << "Overflow Detected!" << std::endl;
+        }
+        break;
+
+    default:
+        break;
+    }
 }
--- ALUemul/alu.h
+++ ALUemul/alu.h
@@ -1,10 +1,11 @@
 #ifndef ALU_H
 #define ALU_H
 #include "stdint.h"
+#include <iostream>
 
 class ALU{
     public:
         uint32_t add(uint32_t, uint32_t);
-        bool didOverflow(uint32_t, uint32_t, uint32_t, int);
+        void didOverflow(uint32_t, uint32_t, uint32_t, int);
 };
 #endif
