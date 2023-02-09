a66d4dfb7e9b63241e2b4742aef2178ed9d0c4e0
brandonGHUB <112513361+brandonGHUB@users.noreply.github.com>
2023-02-09 10:11:58 -0800

    overflow for add detection

--- ALUemul/driver.cpp
+++ ALUemul/driver.cpp
@@ -1,17 +1,20 @@
 #include "alu.h"
-#include <iostream>
 
 int main(){
     ALU testALU;
     uint32_t add1;
     uint32_t add2;
+
     std::cout << "Enter a 32 bit unsigned hex value" << std::endl; 
     std::cin >> std::hex >> add1;
     std::cout << "Entered : " << std::dec << add1 << " (translated to decimal)" << std::endl;
+
     std::cout << "Enter another 32 bit unsigned hex value" << std::endl;
     std::cin >> std::hex >> add2;
     std::cout << "Entered : " << std::dec << add2 << " (translated to decimal)" << std::endl;
+
     uint32_t result = testALU.add(add1, add2);
+
     std::cout << std::hex << result << std::endl;
     std::cout << "Returned : " << std::dec << result << " (translated to decimal)" << std::endl;
 }
