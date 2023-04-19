5494c322f02d17d25762be9644d9e5883723da2d
brandonGHUB <112513361+brandonGHUB@users.noreply.github.com>
2023-04-19 13:18:20 -0700

    add register mecahnism dusahdniasodiwajslikd

--- ALUemul/driver.cpp
+++ ALUemul/driver.cpp
@@ -5,12 +5,16 @@
 // Name: Brandon Ramirez
 
 
+// What to do;
+// parse 2 registers as inputs
+// deal with CMP and TST
+
 int main(int argc, char *argv[]){
     ALU testALU;
     uint32_t operator1;
     uint32_t operator2;
 
-    uint32_t r0, r1, r2, r3, r4, r5, r6, r7;
+    uint32_t registers[8] = { 0 };
 
     std::string inputString;
     std::string copyString;
@@ -126,6 +130,29 @@ int main(int argc, char *argv[]){
             std::cout << "Unknown Operation! Check README" << std::endl;
             return 1;
         }
+
+        if(reg.substr(0,2).compare("R0") == 0){
+            registers[0] = result;
+        }else if(reg.substr(0,2).compare("R1") == 0){
+            registers[1] = result;
+        }else if(reg.substr(0,2).compare("R2") == 0){
+            registers[2] = result;
+        }else if(reg.substr(0,2).compare("R3") == 0){
+            registers[3] = result;
+        }else if(reg.substr(0,2).compare("R4") == 0){
+            registers[4] = result;
+        }else if(reg.substr(0,2).compare("R5") == 0){
+            registers[5] = result;
+        }else if(reg.substr(0,2).compare("R6") == 0){
+            registers[6] = result;
+        }else if(reg.substr(0,2).compare("R7") == 0){
+            registers[7] = result;
+        }
+
+        for(int i = 0; i < 8; i++){
+            std::cout << "r" << i << ": " << std::hex << "0x" << registers[i] << "   ";
+        }
+        std::cout << std::endl;
         std::cout << copyString << ": " << std::hex << "0x" << result << std::endl;
         // output flags
         testALU.outputNZ();
